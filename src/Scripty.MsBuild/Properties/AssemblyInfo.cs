3513bcf28c889f1620186904736f9c3b4326b795
KW <krzysztof.wojcicki@schibsted.pl>
2018-08-06 11:50:59 +0200

    Updated MsBuild

--- src/Scripty.MsBuild/Properties/AssemblyInfo.cs
+++ src/Scripty.MsBuild/Properties/AssemblyInfo.cs
@@ -2,7 +2,5 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Scripty.MsBuild")]
-[assembly: AssemblyDescription("")]
 [assembly: ComVisible(true)]
 [assembly: Guid("02aa19ac-f899-4764-a8b8-d92af080998d")]
--- src/Scripty/Properties/AssemblyInfo.cs
+++ src/Scripty/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-
-[assembly: AssemblyDescription("")]
 [assembly: ComVisible(true)]
 [assembly: Guid("742c7885-f583-4cdf-b594-246e10a30050")]
